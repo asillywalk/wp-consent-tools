97d5f091cb4659218c0d9f710c6459d17c700b54
andi <andi@asillywalk.de>
2023-03-20 21:00:52 +0100

    [fix] Tier selection: Consumers need to parse the string to an array

--- src/Service/ConsentTools.php
+++ src/Service/ConsentTools.php
@@ -149,10 +149,10 @@ class ConsentTools
                 $this->getFieldName(ConfigFields::SERVICE_TIER),
                 __('Service Consent Tier', $this->textDomain),
             )->set_options([
-                'Red (necessary) – 0' => 0,
-                'Amber (anonymous & UX) – 1' => 1,
-                'Green (marketing & analytics) – 2' => 2,
-            ])->set_default_value(2),
+                'Red (necessary) – 0' => '0',
+                'Amber (anonymous & UX) – 1' => '1',
+                'Green (marketing & analytics) – 2' => '2',
+            ])->set_default_value('2'),
             Field::make_text(
                 $this->getFieldName(ConfigFields::SERVICE_CATEGORY),
                 __('Category', $this->textDomain),
